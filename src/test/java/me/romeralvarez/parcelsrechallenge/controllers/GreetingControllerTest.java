d4b1ba57b416a9fe731abcdc36e3a88f6d631c0f
romerenriquealvarezmartinez <romeralvareziv@gmail.com>
2023-11-17 13:01:12 +0000

    test(greeting-controller): Implement MockMvc tests for GreetingController
    - Added a new test for GreetingController using MockMvc.
    - The test verifies the correct response for the endpoint '/greeting/A' with any given string.

--- src/test/java/me/romeralvarez/parcelsrechallenge/controllers/GreetingControllerTest.java
+++ src/test/java/me/romeralvarez/parcelsrechallenge/controllers/GreetingControllerTest.java
@@ -1,2 +1,31 @@
-package me.romeralvarez.parcelsrechallenge.controllers;public class GreetingControllerTest {
+package me.romeralvarez.parcelsrechallenge.controllers;
+
+
+import me.romeralvarez.parcelsrechallenge.services.GreetingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private GreetingService greetingService;
+
+  @Test
+  public void testGreetingForCustomerA() throws Exception {
+    Mockito.when(greetingService.greet("A")).thenReturn("Hi");
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting/A"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Hi"));
+  }
+
 }
